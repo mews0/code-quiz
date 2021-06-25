f9665562aef9731f9e8afe88addd1be9a3f5be83
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-06-25 18:07:40 -0500

    change 'questions' to 'question'

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ let startQuiz = function() {
   let startButtonHandler = function(event) {
     let startTarget = event.target;
     if (startTarget.matches(`.start button`)) {
-      questions();
+      question();
     }
   }
   startButton.addEventListener("click", startButtonHandler);
 }
 
 // Questions
-let questions = function() {
-  const questions = [
+let question = function() {
+  const question = [
     [`Commonly used data types do not include:`, [`strings`, `booleans`, `alerts`, `numbers`]], 
     [`The condition in an if/else statement is enclosed with __________.`, [`quotes`, `curly brackets`, `parenthesis`, `square brackets`]], 
     [`Arrays in JavaScript can be used to store __________.`, [`numbers and strings`, `other arrays`, `booleans`, `all of the above`]], 
@@ -20,24 +20,24 @@ let questions = function() {
     [`A very useful tool used during development and debugging for printing content to the debugger is:`, [`JavaScript`, `terminal/bash`, `for loops`, `console.log`]]
   ];
 
-  for (i = 0; i < questions.length; i++) {
+  for (i = 0; i < question.length; i++) {
 
     // h1 - create, assign text, append
-    let questionsH1 = document.createElement(`h1`);
-    questionsH1.textContent = questions[i][0];
-    document.body.append(questionsH1);
+    let questionH1 = document.createElement(`h1`);
+    questionH1.textContent = question[i][0];
+    document.body.append(questionH1);
 
     // ol - create, append
-    let questionsOl = document.createElement(`ol`);
-    document.body.append(questionsOl);
+    let questionOl = document.createElement(`ol`);
+    document.body.append(questionOl);
 
-    for (j = 0; j < questions[i][1].length; j++) {
+    for (j = 0; j < question[i][1].length; j++) {
       
       // li - create, assign, append (how do I convert these to buttons that look like the mock-up?)
-      let questionsLi = document.createElement(`li`);
-      questionsLi.textContent = questions[i][1][j];
-      questionsLi.innerHTML = `<button>` + questions[i][1][j] + `</button>`;
-      questionsOl.append(questionsLi);
+      let questionLi = document.createElement(`li`);
+      questionLi.textContent = question[i][1][j];
+      questionLi.innerHTML = `<button>` + question[i][1][j] + `</button>`;
+      questionOl.append(questionLi);
     }
   }
 }
