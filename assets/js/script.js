5259207dc21965e3a79589a8875ea45661544c5b
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-06-25 10:11:00 -0500

    add questions

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const questions = [
+  [`Commonly used data types do not include:`, [`strings`, `booleans`, `alerts`, `numbers`]], 
+  [`The condition in an if/else statement is enclosed with __________.`, [`quotes`, `curly brackets`, `parenthesis`, `square brackets`]], 
+  [`Arrays in JavaScript can be used to store __________.`, [`numbers and strings`, `other arrays`, `booleans`, `all of the above`]], 
+  [`String values must be enclosed within __________ when being assigned to variables.`, [`commas`, `curly brackets`, `quotes`, `parenthesis`]], 
+  [`A very useful tool used during development and debugging for printing content to the debugger is:`, [`JavaScript`, `terminal/bash`, `for loops`, `console.log`]]
+];
+
+ 
