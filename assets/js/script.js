2c600b0e71dbfbc396e04d0b1ab8e3b342059564
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-06-26 19:52:34 -0500

    add true/false values to question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,11 @@ let startQuiz = function() {
 // Questions
 let takeQuiz = function() {
   const question = [
-    [`Commonly used data types do not include:`, [`strings`, `booleans`, `alerts`, `numbers`]], 
-    [`The condition in an if/else statement is enclosed with __________.`, [`quotes`, `curly brackets`, `parenthesis`, `square brackets`]], 
-    [`Arrays in JavaScript can be used to store __________.`, [`numbers and strings`, `other arrays`, `booleans`, `all of the above`]], 
-    [`String values must be enclosed within __________ when being assigned to variables.`, [`commas`, `curly brackets`, `quotes`, `parenthesis`]], 
-    [`A very useful tool used during development and debugging for printing content to the debugger is:`, [`JavaScript`, `terminal/bash`, `for loops`, `console.log`]]
+    [`Commonly used data types do not include:`, [`strings`, `booleans`, `alerts`, `numbers`], [false, false, true, false]], 
+    [`The condition in an if/else statement is enclosed with __________.`, [`quotes`, `curly brackets`, `parenthesis`, `square brackets`], [false, false, true, false]], 
+    [`Arrays in JavaScript can be used to store __________.`, [`numbers and strings`, `other arrays`, `booleans`, `all of the above`], [false, false, false, true]], 
+    [`String values must be enclosed within __________ when being assigned to variables.`, [`commas`, `curly brackets`, `quotes`, `parenthesis`], [false, false, true, false]], 
+    [`A very useful tool used during development and debugging for printing content to the debugger is:`, [`JavaScript`, `terminal/bash`, `for loops`, `console.log`], [false, false, false, true]]
   ];
 
   for (i = 0; i < question.length; i++) {
